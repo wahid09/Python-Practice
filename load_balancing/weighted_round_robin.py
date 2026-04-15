8ad133e6735ea29ad884d0b39e39d9133b5d2802
wahidpksf <wahid.pksf@gmail.com>
2026-04-15 15:37:39 +0600

    Load Balanching

---
+++ load_balancing/weighted_round_robin.py
@@ -0,0 +1,26 @@
+class WeightedRoundRobin:
+    def __init__(self, servers, weights):
+        self.servers = servers
+        self.weights = weights
+        self.current_index = -1
+        self.current_weight = 0
+
+    def get_next_server(self):
+        while True:
+            self.current_index = (self.current_index + 1) % len(self.servers)
+            if self.current_index == 0:
+                self.current_weight -= 1
+                print(F"The weight of server {self.servers[self.current_index]} is {self.weights[self.current_index]}")
+                if self.current_weight <= 0:
+                    self.current_weight = max(self.weights)
+            if self.weights[self.current_index] >= self.current_weight:
+                return self.servers[self.current_index]
+
+# Example usage
+servers = ["Server1", "Server2", "Server3"]
+weights = [5, 1, 1]
+load_balancer = WeightedRoundRobin(servers, weights)
+
+for i in range(7):
+    server = load_balancer.get_next_server()
+    print(f"Request {i + 1} -> {server}")
